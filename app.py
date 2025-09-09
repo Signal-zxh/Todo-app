e453ad45a469a0bb5b170e61770766810763ec23
赵昕昊 <zhaoxinhao@zhaoxinhaos-MacBook-Air.local>
2025-09-09 19:53:17 +0800

    Initial commit: basic Flask todo app with add task functionality

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+#set database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+#Task model
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks = tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task_content = request.form['content']
+    new_task = Task(content=task_content)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect('/')
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
