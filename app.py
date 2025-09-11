924905507d60027446a2197d50c895ad719e432d
zxh <zhao_xinhao@foxmail.com>
2025-09-11 11:08:10 +0800

    commit5: register and login function

--- app.py
+++ app.py
@@ -1,26 +1,64 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin,LoginManager,login_user,login_required,logout_user,current_user
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '981008'
 #set database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#initializate Flask-Login
+login_manager=LoginManager()
+login_manager.init_app(app)
+login_manager.login_view='login'
+
 db = SQLAlchemy(app)
+
 #Task model
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     done = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+#User model
+class User(UserMixin,db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class LoginForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    submit = SubmitField('登录')
+
+class RegisterForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired(), Length(min=4, max=20)])
+    password = PasswordField('密码', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('注册')
 
 @app.route('/')
+@login_required
 def index():
-    tasks = Task.query.all()
+    tasks = Task.query.filter_by(user_id=current_user.id).all()
     return render_template('index.html', tasks = tasks)
 
 @app.route('/add', methods=['POST'])
+@login_required
 def add():
     task_content = request.form['content']
-    new_task = Task(content=task_content)
+    new_task = Task(content=task_content, user_id=current_user.id)
     db.session.add(new_task)
     db.session.commit()
     return redirect('/')
@@ -38,6 +76,38 @@ def toggle(id):
     task.done = not task.done
     db.session.commit()
     return redirect('/')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html',form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('index'))
+        flash('无效的用户名或密码')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 with app.app_context():
     db.create_all()
